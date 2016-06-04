dd43591ede478f2906c3fda3dfb88ad2e15bcbe1
mhaytmyr <mhaytmyr@fnal.gov>
2016-06-03 22:53:30 -0500

    Working on fit

--- Planicon_Analyzer.C
+++ Planicon_Analyzer.C
@@ -9,6 +9,7 @@
 #include "TGraph.h"
 #include "TCanvas.h"
 #include "TSystem.h"
+#include "TProfile.h"
 
 const int sampleSize = 1024;
 
@@ -49,6 +50,7 @@ int findMin(int n, double *a)
   return loc;
 }
 
+
 void analyze_MCP(char *filename)
 {
 	//read input file
@@ -73,10 +75,82 @@ void analyze_MCP(char *filename)
 
 
 	TCanvas *c1 = new TCanvas("c1","Dynamic Filling Example",200,10,700,500);
+	TH2F *Ch3Dynamic = new TH2F("Ch3Dynamic","",40000,-200,200,100,-0.6,0.2); //ns
+
+	Ch3Dynamic->GetXaxis()->SetTitle("Pulse time (ns)");
+	Ch3Dynamic->GetYaxis()->SetTitle("Pulse amplitude (V)");
+	Ch3Dynamic->SetStats(0000);
+	Ch3Dynamic->SetMarkerStyle(20);
+	Ch3Dynamic->SetMarkerColor(2);
+	Ch3Dynamic->SetMarkerSize(1);
+
+
+	//read all entries and fill the histograms
+   	Long64_t nentries = t1->GetEntries();
+	//for (Long64_t iEntry=0; iEntry<nentries; iEntry++) 
+	for (Long64_t iEntry=0; iEntry<5; iEntry++) 
+	{
+     		t1->GetEntry(iEntry);
+
+		std::cout<<"Entry Number "<<iEntry<<std::endl;
+		int ch1Min = findMin(sampleSize,b1_ch1);
+		int ch2Min = findMin(sampleSize,b1_ch2);
+		int ch3Min = findMin(sampleSize,b1_ch3);
+		int ch4Min = findMin(sampleSize,b1_ch4);
+
+		for(int m=0; m<1024; m++)
+                {
+                        Ch3Dynamic->Fill(b1_t3[m],b1_ch3[m]);
+                }
+
+		//TF1 *f1 = new TF1("f1","[0]+[1]*x",b1_t3[ch3Min-5],b1_t3[ch3Min]);
+		TProfile *prof = Ch3Dynamic->ProfileX();
+		prof->Fit("pol1","","",b1_t3[ch3Min-100],b1_t3[ch3Min+100]);		
+		prof->SetMarkerColor(kBlue);
+
+                //Zoom to the +/-10 marks of the minimum value
+                Ch3Dynamic->GetXaxis()->SetRangeUser(b1_t3[ch3Min-50],b1_t3[ch3Min+30]);
+                std::cout<<"Min channel "<<b1_t3[ch3Min]<<std::endl;
+                std::cout<<"Limits "<<b1_t3[ch3Min-10]<<" : "<<b1_t3[ch3Min+10]<<std::endl;
+
+                Ch3Dynamic->Draw();
+		prof->Draw("same");
+                c1->Modified();
+                c1->Update();
+                Ch3Dynamic->Reset();
+		prof->Reset();
+                gSystem->Sleep(1500);  //in mictroseconds
+
+
+	}
+
+}
+
+void plot_MCP(char *filename)
+{
+	//read input file
+	TFile *fIn = new TFile(filename);
+	TTree *t1 = (TTree*)fIn->Get("rec");
+
+	//decalare variables to hold 
+	Double_t b1_ch1[1024], b1_ch2[1024], b1_ch3[1024], b1_ch4[1024];
+	Double_t b1_t1[1024], b1_t2[1024], b1_t3[1024], b1_t4[1024];
+
+	//get from branch
+	t1->SetBranchAddress("b1_w1",&b1_ch1);
+   	t1->SetBranchAddress("b1_w2",&b1_ch2);
+   	t1->SetBranchAddress("b1_w3",&b1_ch3);
+   	t1->SetBranchAddress("b1_w4",&b1_ch4);
+
+	t1->SetBranchAddress("b1_t1",&b1_t1);
+   	t1->SetBranchAddress("b1_t2",&b1_t2);
+   	t1->SetBranchAddress("b1_t3",&b1_t3);
+   	t1->SetBranchAddress("b1_t4",&b1_t4);
+
+
+	TCanvas *c1 = new TCanvas("c1","Dynamic Filling Example",200,10,700,500);
 	//decalre histograms
 	TH1F *delTMax = new TH1F("delTMax","Delta T btw maxima",10000,-10,10); //ns
-	//TH2F *Ch3Dynamic = new TH2F("Ch3Dynamic","",1024,0,1024,100,-1,0.4); //channel #
-	//TH2F *Ch3Dynamic = new TH2F("Ch3Dynamic","",40000,-200,200,100,-0.6,0.4); //ns
 	TH2F *Ch3Dynamic = new TH2F("Ch3Dynamic","",40000,-200,200,100,-0.6,0.2); //ns
 
 	Ch3Dynamic->GetXaxis()->SetTitle("Pulse time (ns)");
